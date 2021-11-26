17e03e60a109d89a2fac0730c0c35d636a6c25d6
tomknox00 <64932850+tomknox00@users.noreply.github.com>
2021-11-25 19:03:55 -0500

    added unit tests for decimal values

--- app/src/test/java/com/example/simplecalculator_lab1/ExampleUnitTest.java
+++ app/src/test/java/com/example/simplecalculator_lab1/ExampleUnitTest.java
@@ -12,29 +12,29 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-        assertEquals(-1, -3 + 2);
-        assertEquals(0.7, 0.5 + 0.2, 0.01);
+        assertEquals("Integer addition: 2+2 ",4, 2 + 2);
+        assertEquals("negative Integer addition: -3+2 ",-1, -3 + 2);
+        assertEquals("decimal addition: 0.5+0.2 ",0.7, 0.5 + 0.2, 0.01);
 
     }
     @Test
     public void subtraction_isCorrect() {
-        assertEquals(35, 70 - 35);
-        assertEquals(-105, -70 - 35);
-        assertEquals(-3.7, 5 - 8.7, 0.01);
+        assertEquals("Integer subtraction: 70-35 ",35, 70 - 35);
+        assertEquals("Integer subtraction with negative values: -70-35 ",-105, -70 - 35);
+        assertEquals("subtraction with decimal values: 5-8.7 ",-3.7, 5 - 8.7, 0.01);
     }
 
     @Test
     public void multiplication_isCorrect() {
-        assertEquals(72, 9 * 8);
-        assertEquals(-6, -2 * 3);
-        assertEquals(4.5, 3 * 1.5, 0.01);
+        assertEquals("Integer multiplication: 9*8 ",72, 9 * 8);
+        assertEquals("Integer multiplication with negative values: -2*3 ",-6, -2 * 3);
+        assertEquals("multiplication with decimal values: 3*1.5 ",4.5, 3 * 1.5, 0.01);
     }
     @Test
     public void division_isCorrect() {
-        assertEquals(1, 9 / 9);
-        assertEquals(-12, 144/-12 );
-        assertEquals(0.5747, 5 / 8.7, 0.0001);
+        assertEquals("Integer division: 9/9 ",1, 9 / 9);
+        assertEquals("Integer division: 144/-12 ",-12, 144/-12 );
+        assertEquals("Division with decimal values: 5/8.7 ",0.5747, 5 / 8.7, 0.0001);
     }
 
 }
